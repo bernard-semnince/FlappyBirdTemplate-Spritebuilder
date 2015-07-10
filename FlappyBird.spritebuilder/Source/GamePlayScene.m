741fed0d0c55a7b2b189ce2545f3908fef3cae3d
Matteo <matteo.marinucci@ampush.com>
2015-07-10 14:03:07 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,12 +11,18 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
                              
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    if(timeSinceObstacle > 2.0f) {
+        [self addObstacle];
+        timeSinceObstacle = 0.0f;
+    }
     
 }
 
