768b417dabf1184f2375d88833bd0f4bf86997ac
Matteo <matteo.marinucci@ampush.com>
2015-07-10 13:54:41 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,24 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+                             
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    
 }
 
 // put new methods here
+-(void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    
+    [character flap];
+    
+}
+
+
 
 @end
